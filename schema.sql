abaa39da009d3d860e293776f03f9eb4a2c31b42
James Geib <jpgeib2@gmail.com>
2020-04-02 12:23:21 -0700

    fixed spelling error in schema.sql

--- schema.sql
+++ schema.sql
@@ -2,13 +2,13 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 CREATE TABLE department(
-    id INTEGER AUTO_INCRAMENT NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCRAMENT NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER,
@@ -16,7 +16,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCRAMENT NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
