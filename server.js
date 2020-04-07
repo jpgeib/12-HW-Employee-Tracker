ecca51a49c592b9cc60099be2628f7f8b592bae4
James Geib <jpgeib2@gmail.com>
2020-04-07 14:34:22 -0700

    built main initialize function

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Boilerplate code
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
@@ -11,10 +12,41 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected as id " + connection.threadId + "\n")
+    console.log("Connected as id " + connection.threadId + "\n");
+    initialize();
 });
 
+//My functions
+
 function initialize() {
-    
-}
+    inquirer.prompt({
+        name: "MainMenu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager",
+    "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles"]
+    }).then(function(answer) {
+        if (answer.MainMenu === "View All Employees") {
+            viewAllEmployees();
+        } else if(answer.MainMenu === "View All Employees by Department") {
+            employeesByDept();
+        } else if(answer.MainMenu === "View All Employees by Manager") {
+            employeesByManager();
+        } else if(answer.MainMenu === "Add Employee") {
+            addEmployee();
+        } else if(answer.MainMenu === "Remove Employee") {
+            removeEmployee();
+        } else if(answer.MainMenu === "Update Employee Role") {
+            updateEmployeeRole();
+        } else if(answer.MainMenu === "Update Employee Manager") {
+            updateEmployeeManager();
+        } else if(answer.MainMenu === "View All Roles") {
+            viewAllRoles();
+        } else {
+            connection.end();
+        };
+    });
+};
+
+
 
