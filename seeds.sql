6dec1abe3f7f1091eab497b7f076cb7121c916ca
James Geib <jpgeib2@gmail.com>
2020-04-07 14:13:58 -0700

    schema is fixed

--- seeds.sql
+++ seeds.sql
@@ -11,6 +11,6 @@ VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 1
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, 3), ("Mike", "Chan", 2, 1), ("Ashley", "Rodriguez", 3, null),
 ("Kevin", "Tupik", 4, 3), ("Malia", "Brown", 5, null), ("Sarah", "Lourd", 6, null),
-("Tom", "Allen", 7, 7), ("Christian", "Eckenrode", 3, 2);
+("Tom", "Allen", 7, 6), ("Christian", "Eckenrode", 3, 2);
 
 
